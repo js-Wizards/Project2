e342c0b91229c4712b9ffde4561a5d2769bfa7a9
Bill W <42258241+wwunsch23@users.noreply.github.com>
2019-01-08 17:21:18 -0600

    Minor tweaks to style

--- routes/userHtmlRoutes.js
+++ routes/userHtmlRoutes.js
@@ -5,11 +5,10 @@ var path = require("path");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/events");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
@@ -17,7 +16,7 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/events");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
@@ -25,7 +24,7 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    console.log(isAuthenticated);
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
-
 };
