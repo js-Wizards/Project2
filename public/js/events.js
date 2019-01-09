c8b0240776e9a99c648eb58c88a000762d87260c
Bill W <42258241+wwunsch23@users.noreply.github.com>
2019-01-09 12:13:28 -0600

    Added Remove from My Events functionality

--- public/js/events.js
+++ public/js/events.js
@@ -1,28 +1,44 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
     $(".member-name").attr("data-userid", data.id);
   });
-});
 
-
-$(".add_to_my_events").on("click", function () {
-  const userID = $(".member-name").attr("data-userid");
-  const eventID = $(this).attr("data-eventID");
-  // console.log("this is this", this);
-  // console.log("clicked add to my events button");
-  console.log(userID);
-  console.log(eventID);
-  $.post("/api/events", {
+  $(".add_to_my_events").on("click", function () {
+    const userID = $(".member-name").attr("data-userid");
+    const eventID = $(this).attr("data-eventID");
+    // console.log("this is this", this);
+    // console.log("clicked add to my events button");
+    console.log(userID);
+    console.log(eventID);
+    $.post("/api/events", {
       UserId: userID,
       eventId: eventID
-  }).then(function (myData) {
+    }).then(function (myData) {
       // console.log("posting");
       // console.log(myData);
       // Reload the page to get the updated list
       location.reload();
+    });
   });
-});
 
+  $(".delete_my_events").on("click", function () {
+    const userID = $(".member-name").attr("data-userid");
+    const eventID = $(this).attr("data-eventID");
+    // console.log("this is this", this);
+    // console.log("clicked add to my events button");
+    console.log(userID);
+    console.log(eventID);
+    $.ajax({
+      method: "DELETE",
+      url: "api/events/"+ eventID + "/user/" + userID
+    }).then(function (myData) {
+      console.log("deleted", myData);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+});
--- routes/event-apiRoutes.js
+++ routes/event-apiRoutes.js
@@ -7,118 +7,127 @@ module.exports = function (app) {
   app.delete("/api/events", function (request, response) {
     let date = moment().format("YYYY-MM-DD");
     db.Event.destroy({
-        where: {
-          uuid: {
-            [Op.or]: {
-              [Op.gt]: 'A',
-              [Op.lt]: 'A'
-            }
-          },
-          // isUserCreated: {
-          //   [Op.eq]: 0
-          // },
-          startDate: {
-            [Op.lt]: date
+      where: {
+        uuid: {
+          [Op.or]: {
+            [Op.gt]: 'A',
+            [Op.lt]: 'A'
           }
+        },
+        // isUserCreated: {
+        //   [Op.eq]: 0
+        // },
+        startDate: {
+          [Op.lt]: date
         }
-      })
+      }
+    })
       .then(deletedData => {
         response.json(deletedData);
       });
   });
 
-  app.delete("/api/events/:id", function (request, response) {
-    db.Event.destroy({
+  app.delete("/api/events/:eventId/user/:id", function (request, response) {
+    db.Event.findOne({
+      where: {
+        eventId: request.params.eventId
+      }
+    }).then(function (dbEvent) {
+      //console.log("Delete", dbEvent);
+      db.SavedEvent.destroy({
         where: {
-          id: request.params.id
+          UserId: request.params.id,
+          EventUuid: dbEvent.uuid
         }
       })
-      .then(function (deletedData) {
-        response.json(deletedData);
-      });
+        .then(function (deletedData) {
+          response.json(deletedData);
+        });
+    });
   });
 
-  //GET route for getting all of the public events by category
-  app.get("/api/events/category/:category", function (request, response) {
-    db.Category.findAll({
-      where: {
-        name: request.params.category
-      },
-      include: [{
-        model: db.Event,
+
+    //GET route for getting all of the public events by category
+    app.get("/api/events/category/:category", function (request, response) {
+      db.Category.findAll({
+        where: {
+          name: request.params.category
+        },
+        include: [{
+          model: db.Event,
+          where: {
+            isUserCreated: 0
+          },
+          required: true,
+        }],
+        order: [
+          [db.Event, "startDate", "ASC"],
+          [db.Event, "startTime", "ASC"]
+        ]
+      }).then(function (dbEvent) {
+        response.json(dbEvent);
+      });
+    });
+
+    //GET route for getting all of the public events
+    app.get("/api/events", function (request, response) {
+      db.Event.findAll({
         where: {
           isUserCreated: 0
         },
-        required: true,
-      }],
-      order: [
-        [db.Event, "startDate", "ASC"],
-        [db.Event, "startTime", "ASC"]
-      ]
-    }).then(function (dbEvent) {
-      response.json(dbEvent);
+        order: [
+          ["startDate", "ASC"],
+          ["startTime", "ASC"]
+        ]
+      }).then(function (dbEvent) {
+        response.json(dbEvent);
+      });
     });
-  });
 
-  //GET route for getting all of the public events
-  app.get("/api/events", function (request, response) {
-    db.Event.findAll({
-      where: {
-        isUserCreated: 0
-      },
-      order: [
-        ["startDate", "ASC"],
-        ["startTime", "ASC"]
-      ]
-    }).then(function (dbEvent) {
-      response.json(dbEvent);
-    });
-  });
-
-  // POST route for saving a new user event
-  app.post("/api/events", function (request, response) {
-    console.log("this is the request body", request.body);
-    db.Event.findOne({
+    // POST route for saving a new user event
+    app.post("/api/events", function (request, response) {
+      console.log("this is the request body", request.body);
+      db.Event.findOne({
         where: {
           eventId: request.body.eventId
         }
       })
-      .then(function (dbEvent) {
-        console.log(dbEvent);
-        //console.log(newEvent);
-        //console.log("This is after the find")
-        db.SavedEvent.create({
+        .then(function (dbEvent) {
+          console.log(dbEvent);
+          //console.log(newEvent);
+          //console.log("This is after the find")
+          db.SavedEvent.create({
             EventUuid: dbEvent.uuid,
             UserId: request.body.UserId
           })
-          .then(function (dbSavedEvent) {
-            console.log(dbSavedEvent);
-            response.json(dbSavedEvent);
-          })
-      })
-  });
-
-  app.get("/api/events/user/:id", function (request, response) {
-    // var query = {};
-    // if (req.query.author_id) {
-    //   query.AuthorId = req.query.author_id;
-    // }
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.User.findAll({
-      where: {
-        id: request.params.id
-      },
-      include: [{
-        model: db.Event,
-        attributes: ["eventName", "venueName", "addressLine1", "addressLine2", "city", "state", "postalCode", "startDate", "startTime"]
-      }]
-    }).then(function (dbEvent) {
-      response.json(dbEvent[0].Events);
+            .then(function (dbSavedEvent) {
+              console.log(dbSavedEvent);
+              response.json(dbSavedEvent);
+            })
+        })
     });
-  });
+
+    // app.get("/api/events/user/:id", function (request, response) {
+    //   // var query = {};
+    //   // if (req.query.author_id) {
+    //   //   query.AuthorId = req.query.author_id;
+    //   // }
+    //   // Here we add an "include" property to our options in our findAll query
+    //   // We set the value to an array of the models we want to include in a left outer join
+    //   // In this case, just db.Author
+    //   db.User.findAll({
+    //     where: {
+    //       id: request.params.id
+    //     },
+    //     include: [{
+    //       model: db.Event,
+    //       attributes: ["eventName", "venueName", "addressLine1", "addressLine2", "city", "state", "postalCode", "startDate", "startTime"]
+    //     }]
+    //   }).then(function (dbEvent) {
+    //     response.json(dbEvent[0].Events);
+    //   });
+    // });
 
 
 
-}
+  }
